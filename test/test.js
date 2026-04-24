ea57def647334e7fea53ceaf089ce45ae710adc8
Carlos Dantas <carlos.d.dantas6@aluno.senai.br>
2026-04-24 09:28:31 -0300

    Add: Começando testes

---
+++ test/test.js
@@ -0,0 +1,15 @@
+
+
+try{
+
+
+
+}catch(erro){
+
+    let text = `
+    <strong> Nome do erro:</strong> ${erro.name} <br />
+    <strong> Mensagem: </strong> ${erro.message} <br />
+    <strong> Stack: </strong> <span> ${erro.stack} <span/>
+    `;
+    document.body.innerHTML = text
+};
