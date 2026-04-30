ed2392dfad1f9a77a7127243dd027fe25c706462
Carlos Dantas <carlos.d.dantas6@aluno.senai.br>
2026-04-30 11:18:47 -0300

    Add: projeto cep em andamento

--- test/test.js
+++ test/test.js
@@ -62,6 +62,36 @@
 
 // =========================================================== //
 
-fetch('https://viacep.com.br/ws/01001000/json/')
+document.querySelector('input__cep')
 
+const url = `https://viacep.com.br/ws/${cep}/json/`
+const consulta = fetch(url)
+console.log(consulta)
+consulta.then(resposta => {
 
+    if(!resposta.ok){
+        throw new Error('Erro na Requisição')
+    }
+    console.log(dados)
+})
+
+.then((resposta) => {
+    return resposta.json()
+})
+
+.then((dados) => {
+    console.log(dados)
+})
+.catch((error) => {
+    console.warn(error)
+}
+)
+
+function buscarComThen(cep) {
+
+    const CampoCep = document.getElementById('resultado__cep');
+
+    const cepCapturado = document.querySelector('cep')
+    
+    
+}
