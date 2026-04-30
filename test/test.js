76c4b478d218e292341b64ab38664a8d872d2a4a
Carlos Dantas <carlos.d.dantas6@aluno.senai.br>
2026-04-30 10:35:14 -0300

    Add: Criando uma função de erros para testala em qualuqer problema

--- test/test.js
+++ test/test.js
@@ -1,18 +1,67 @@
-const carregarComponente = async(caminho, container) =>{
-    try{
+// =========================================================== //
 
-        const resposta = await fetch(caminho);
+// const compo = document.querySelector('body')
 
-        if(!resposta.ok){
-            throw new Error('Error ao carregar componente')
-        }
+// const carregarComponente = async(caminho, container) =>{
+//     try{
 
-        const html = await resposta.text();
+//         const resposta = await fetch(caminho);
 
-        container.innerHTML = html;
+//         if(!resposta.ok){
+//             throw new Error('Error ao carregar componente')
+//         }
 
-    }catch(erro){
-        console.error(erro)
+//         const html = await resposta.text();
 
-    };                                        
-};
+//         container.innerHTML = html;
+
+//     }catch(erro){
+//         console.warn(erro)
+
+//     };                                        
+// };
+// url = 'c-test.html';
+// Componente = document.querySelector('#app');
+// carregarComponente();
+
+// =========================================================== //
+
+// try {
+// let saldo = 100;
+
+// if (saldo < 200){
+
+//     throw geradorErros(1001, 'VALIDACAO', 'Dinheiro insuficiente') /* Chamei a função gerador de erros e setei os parametros criados nela em ordem */
+// };
+
+// } catch (erro) {
+
+//     let text = `
+//     <strong>Nome do erro: </strong> ${erro.name} <br />
+//     <strong>Mensagem: </strong> ${erro.message} <br />
+//     <strong>Código: </strong> ${erro.codigo} <br />
+//     `;
+
+//     document.body
+
+// };
+
+// // 
+// function geradorErros(codigo, tipo, mensagem) {
+
+//     let erro =  new Error(mensagem)
+    
+//     erro.codigo = codigo
+//     erro.tipo = tipo
+
+//     return erro
+
+// }
+
+// console.log(geradorErros)
+
+// =========================================================== //
+
+fetch('https://viacep.com.br/ws/01001000/json/')
+
+
